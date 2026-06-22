4d3839776f70a0de59c4790ff068a99e65000c27
linstudio-design <linstudio.au@gmail.com>
2026-06-23 00:45:41 +1000

    Add files via upload

---
+++ global.js
@@ -0,0 +1,36 @@
+body {
+  margin: 0;
+  padding: 0;
+  background-color: #f3f3f3;
+  color: #000000;
+  font-family: 'Inter', sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+
+.container {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 0 20px;
+  box-sizing: border-box;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
+  transition: opacity 0.2s ease;
+}
+
+a:hover {
+  opacity: 0.7;
+}
+
+img {
+  max-width: 100%;
+  height: auto;
+  display: block;
+}
+
+p {
+  margin: 0;
+}
